68d53361e57fff7c0600a2d0160e2aee493b5f04
Gery711KO <kocsis.gery@gmail.com>
2021-12-23 13:05:23 +0100

    -||-

--- app/src/main/java/hu/kocsisgeri/bitraptors/data/logic/Filter.kt
+++ app/src/main/java/hu/kocsisgeri/bitraptors/data/logic/Filter.kt
@@ -16,7 +16,7 @@ data class Filter(
             it.age >= this.minAge ?: 0 &&
                     it.age <= this.maxAge ?: 150 &&
                     (it.baseIllnesses.lowercase().contains(this.keyword?.lowercase() ?: "") ||
-                    it.sex.lowercase().contains(this.keyword?.lowercase() ?: ""))
+                            it.sex.lowercase().contains(this.keyword?.lowercase() ?: ""))
         }
     }
 }
